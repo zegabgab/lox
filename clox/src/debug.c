e79792341a69173c163c9c426330e4cf3de9bb0e
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-08-16 17:44:52 +0200

    Chapter 15 - A Virtual Machine

--- clox/src/debug.c
+++ clox/src/debug.c
@@ -26,18 +26,29 @@ static int simpleInstruction(const char *name, int offset) {
 
 int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d ", offset);
-    if (offset > 0 && getLine(chunk, offset) == getLine(chunk, offset - 1)) {
+    int line = getLine(chunk, offset);
+    if (offset > 0 && line == getLine(chunk, offset - 1)) {
         printf("   | ");
     } else {
-        printf("%4d ", getLine(chunk, offset));
+        printf("%4d ", line);
     }
 
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
+        case OP_ADD:
+            return simpleInstruction("OP_ADD", offset);
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_DIVIDE:
+            return simpleInstruction("OP_DIVIDE", offset);
+        case OP_MULTIPLY:
+            return simpleInstruction("OP_MULTIPLY", offset);
+        case OP_NEGATE:
+            return simpleInstruction("OP_NEGATE", offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
+        case OP_SUBTRACT:
+            return simpleInstruction("OP_SUBTRACT", offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
--- clox/src/main.c
+++ clox/src/main.c
@@ -1,18 +1,37 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char **argv) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 6.9);
+    int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 420);
     writeChunk(&chunk, constant, 420);
+
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 420);
+    writeChunk(&chunk, constant, 420);
+
+    writeChunk(&chunk, OP_ADD, 420);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 420);
+    writeChunk(&chunk, constant, 420);
+
+    writeChunk(&chunk, OP_DIVIDE, 420);
+
+    writeChunk(&chunk, OP_NEGATE, 420);
 
     writeChunk(&chunk, OP_RETURN, 420);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
